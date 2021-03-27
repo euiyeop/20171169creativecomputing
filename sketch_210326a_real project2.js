887aea6f070e6c522df9664fb0a0c4e32bb0433d
euiyeop <81463992+euiyeop@users.noreply.github.com>
2021-03-27 18:25:58 +0900

    Add files via upload

---
+++ sketch_210326a_real project2.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(1500, 1000);
+  background(0);  
+}
+
+
+function draw() {
+  x = random(0,750); // circle random
+  y = random(0,1000);
+  
+  
+  let size = random(0, 10);
+    fill(random(255), random(255), random(255));
+  rect(x, y, 5 , 20);  //rectangle random
+ 
+  if (mouseIsPressed) {
+    fill(random(255), random(255), random(255));
+    var r = random(1,20);
+    ellipse(mouseX, mouseY, r, r);
+}
+}
